4533183079dced9c97144f1aa840434eb7cbc3b7
PhilPalmer <phil.palmer.email@gmail.com>
2023-04-19 16:59:43 +0100

    Remove rare k-mers

--- tvax/score.py
+++ tvax/score.py
@@ -22,6 +22,8 @@ def add_scores(
     # TODO: Do this in a more efficient way
     if config.weights.frequency:
         kmers_dict = add_frequency_score(kmers_dict, N)
+    if config.conservation_threshold:
+        kmers_dict = remove_rare_kmers(kmers_dict, config.conservation_threshold)
     if config.weights.population_coverage_mhc1:
         kmers_dict = add_population_coverage(kmers_dict, config, "mhc1")
     if config.weights.population_coverage_mhc2:
@@ -62,6 +64,18 @@ def add_frequency_score(kmers_dict: dict, N: int) -> dict:
     """
     for kmer in kmers_dict:
         kmers_dict[kmer]["frequency"] = kmers_dict[kmer]["count"] / N
+    return kmers_dict
+
+
+def remove_rare_kmers(kmers_dict: dict, conservation_threshold: float) -> dict:
+    """
+    Remove k-mers that are not conserved in the target population.
+    """
+    kmers_dict = {
+        kmer: kmers_dict[kmer]
+        for kmer in kmers_dict
+        if kmers_dict[kmer]["frequency"] >= conservation_threshold
+    }
     return kmers_dict
 
 
