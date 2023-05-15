451b2ee367b42a24c3d2ed9b41d5412c1529d313
PhilPalmer <phil.palmer.email@gmail.com>
2023-05-15 13:09:26 +0100

    Compute the total score rather than just the pop cov mhc1

--- tvax/design.py
+++ tvax/design.py
@@ -37,16 +37,24 @@ def find_optimal_path(G: nx.Graph, config) -> list:
     """
     # Load the haplotypes and overlap data
     peptides = [e for e in G.nodes if e not in ["BEGIN", "END"]]
-    mhc_type = "mhc1"
-    hap_freq_path = (
-        config.hap_freq_mhc1_path if mhc_type == "mhc1" else config.hap_freq_mhc2_path
-    )
-    hap_freq, average_frequency = load_haplotypes(hap_freq_path)
-    overlap_haplotypes = load_overlap(peptides, hap_freq, config, mhc_type)
+    hap_freq_path_mhc1 = config.hap_freq_mhc1_path
+    hap_freq_path_mhc2 = config.hap_freq_mhc2_path
+    hap_freq_mhc1, average_frequency_mhc1 = load_haplotypes(hap_freq_path_mhc1)
+    hap_freq_mhc2, average_frequency_mhc2 = load_haplotypes(hap_freq_path_mhc2)
+    overlap_haplotypes_mhc1 = load_overlap(peptides, hap_freq_mhc1, config, "mhc1")
+    overlap_haplotypes_mhc2 = load_overlap(peptides, hap_freq_mhc2, config, "mhc2")
     # Forward loop - compute F(e)
     epitopes = sorted(G.nodes, key=lambda e: G.nodes[e]["pos"][0])
     for e in tqdm(epitopes):
-        F(G, e, config, average_frequency, overlap_haplotypes)
+        F(
+            G,
+            e,
+            config,
+            average_frequency_mhc1,
+            average_frequency_mhc2,
+            overlap_haplotypes_mhc1,
+            overlap_haplotypes_mhc2,
+        )
     # Backward loop - build the path that achieves the maximal score
     path = backward(G)
     return path
@@ -70,8 +78,10 @@ def F(
     G: nx.Graph,
     e: str,
     config,
-    average_frequency: pd.DataFrame = None,
-    overlap_haplotypes: pd.DataFrame = None,
+    average_frequency_mhc1: pd.DataFrame = None,
+    average_frequency_mhc2: pd.DataFrame = None,
+    overlap_haplotypes_mhc1: pd.DataFrame = None,
+    overlap_haplotypes_mhc2: pd.DataFrame = None,
 ) -> float:
     """
     Returns the maximum total score over all paths that end in e
@@ -89,32 +99,90 @@ def F(
         else:
             predecessors = P(G, e)
             if predecessors == ["BEGIN"]:
-                # If the set of predecessors P(e) is empty, then F(e) = pop_coverage(e)
-                Fe = compute_population_coverage(
+                # If the set of predecessors P(e) is empty, then F(e) = compute_score(e)
+                Fe = compute_score(
+                    G,
+                    e,
                     [e],
-                    config.n_target,
                     config,
-                    "mhc1",
-                    average_frequency,
-                    overlap_haplotypes,
+                    average_frequency_mhc1,
+                    average_frequency_mhc2,
+                    overlap_haplotypes_mhc1,
+                    overlap_haplotypes_mhc2,
                 )
             else:
                 # Get all nodes on the optimal path from the start node to the current node
                 path = get_optimal_p(G, e, [e], config)
-                # Then compute the F(e) i.e. population coverage for all of the nodes in the path
-                Fe = compute_population_coverage(
+                # Then compute the F(e) i.e. score for all of the nodes in the path
+                Fe = compute_score(
+                    G,
+                    e,
                     path,
-                    config.n_target,
                     config,
-                    "mhc1",
-                    average_frequency,
-                    overlap_haplotypes,
+                    average_frequency_mhc1,
+                    average_frequency_mhc2,
+                    overlap_haplotypes_mhc1,
+                    overlap_haplotypes_mhc2,
                 )
                 print(f"Computed F({e}) = {Fe} using {len(path)} peptides")
 
         # Save F(e) to the graph for this epitope
         nx.set_node_attributes(G, {e: Fe}, "F(e)")
     return f(G, e, f="F(e)")
+
+
+def compute_score(
+    G: nx.Graph,
+    e: str,
+    path: list,
+    config: EpitopeGraphConfig,
+    average_frequency_mhc1: pd.DataFrame = None,
+    average_frequency_mhc2: pd.DataFrame = None,
+    overlap_haplotypes_mhc1: pd.DataFrame = None,
+    overlap_haplotypes_mhc2: pd.DataFrame = None,
+) -> float:
+    """
+    Returns the maximum total score over all paths that end in e
+    """
+    # If population coverage weights are set compute these scores
+    if config.weights.population_coverage_mhc1:
+        nx.set_node_attributes(
+            G,
+            {
+                e: compute_population_coverage(
+                    path,
+                    config.n_target,
+                    config,
+                    "mhc1",
+                    average_frequency_mhc1,
+                    overlap_haplotypes_mhc1,
+                )
+            },
+            "population_coverage_mhc1",
+        )
+    if config.weights.population_coverage_mhc2:
+        nx.set_node_attributes(
+            G,
+            {
+                e: compute_population_coverage(
+                    path,
+                    config.n_target,
+                    config,
+                    "mhc2",
+                    average_frequency_mhc2,
+                    overlap_haplotypes_mhc2,
+                )
+            },
+            "population_coverage_mhc2",
+        )
+    # Compute the total score by taking a weighted average of the scores
+    score = sum(
+        # Get all of the node attribute scores for this epitope
+        [G.nodes[e][name] * val for name, val in config.weights if val > 0]
+    ) / sum([val for name, val in config.weights if val > 0])
+    # Multiply the score by the clade weight
+    score *= G.nodes[e]["clade_weight"]
+    return score
 
 
 def backward(G: nx.Graph, path: list = []) -> list:
@@ -170,12 +238,12 @@ def cocktail(
         for e in q:
             nx.set_node_attributes(G, {e: 0}, score)
         # Remove F(e) so it's recomputed using the updated scores
-        for (e, d) in G.nodes(data=True):
+        for e, d in G.nodes(data=True):
             del d["F(e)"]
     # Reset to the original scores
     nx.set_node_attributes(G, score_dict, score)
     # Optional: Repeat - iterative refinement
-    if refine:
+    if refine and m > 1:
         Q = iterative_refinement(G, Q, config=config)
     return Q
 
@@ -209,7 +277,7 @@ def iterative_refinement(
             # Reset to the original scores
             nx.set_node_attributes(G, score_dict, score)
             # Remove F(e) so it's recomputed using the updated scores
-            for (e, d) in G.nodes(data=True):
+            for e, d in G.nodes(data=True):
                 del d["F(e)"]
             if Q == prev_Q:
                 return Q
